691c24a6c7825800ce866a33be2e88ff45197b5c
RAHUL RAJ KHADKA <sirrahul9848@gmail.com>
2025-12-31 11:47:41 +0545

    isanagarm fucntion

---
+++ isAnagram.js
@@ -0,0 +1,22 @@
+function isAnagram(s,t) {
+    if(s.length!==t.length)return false;
+
+    const count ={}
+
+    for( const char of s){
+
+        count[char]=(count[char]|| 0) +1;
+    }
+
+    for (const char of t){
+
+        if(!count[char]) return false;
+        count[char]--;
+    }
+
+    return true;
+    
+}
+
+console.log(isAnagram("listen","silent"));
+console.log(isAnagram("rat","car"));
